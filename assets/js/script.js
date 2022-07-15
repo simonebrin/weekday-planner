e29092ad91634835c2c52e2f547ba8c284d3cfd4
Simone Brin <simonebrin@gmail.com>
2022-07-15 10:12:37 -0400

    adding local storage elemenets

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,11 +38,51 @@ checkTime();
 
 // On save, it will take the input the user put in, and save it into localstorage in respect of that timeblock
 // var testEl = document.querySelector('#test')
-var testEl = document.getElementsByClassName('col-sm-10 description')
-// var testEl = document.getElementById('#test')
+// var testEl = document.getElementsByClassName('col-sm-10 description')
+
+var testEl = document.getElementById('#input')
 console.log(testEl)
 localStorage.setItem('10am', 'Go to Work')
 localStorage.setItem('hour-9', 'textarea')
 localStorage.setItem('11am', 'LEavfework')
-// testEl = localStorage.getItem('hour-9')
+testEl = localStorage.getItem('hour-9')
+console.log(testEl)
 
+// function addTextEntry(key, text, isNewEntry) {
+
+//     // Create a textarea element to edit the entry
+//     var textEntry = document.getElementById('input').innerText;
+//     textEntry.placeholder = "(new entry)";
+
+//     // Set the text within the textarea
+//     textEntry.innerText = text;
+
+//     // Add a section to the diary containing the textarea
+//     addSection(key, textEntry);
+
+//     // If this is a new entry (added by the user clicking a button)
+//     // move the focus to the text area to encourage typing
+//     if (isNewEntry) {
+//       textEntry.focus();
+//     }
+
+// document.getElementById("button-9").addEventListener("click", function() {
+//     // TODO: Within the listener function...
+//     // TODO: ...make an item using the text area value
+//     item = makeItem(
+//     "text",
+//     textEntry.value
+//   );
+//     // TODO: ...store the item in local storage using the given key
+//      localStorage.setItem(hour-9, item);
+//      });
+
+//     }
+//     addTextEntry(key, text, isNewEntry)
+
+// localStorage.input = "";
+document.getElementById("input").innerText = localStorage.input;
+console.log(localStorage.input)
+document.getElementById("button-9").addEventListener("click", function(){
+    localStorage.setItem("hour-9", localStorage.input);
+});
