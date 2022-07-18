92d33256ca394cca42e4646ea8ccc7df54ad776f
Simone Brin <simonebrin@gmail.com>
2022-07-18 10:57:58 -0400

    tring to fix buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,13 +40,15 @@ checkTime();
 // var testEl = document.querySelector('#test')
 // var testEl = document.getElementsByClassName('col-sm-10 description')
 
-var testEl = document.getElementById('#input')
-console.log(testEl)
-localStorage.setItem('10am', 'Go to Work')
-localStorage.setItem('hour-9', 'textarea')
-localStorage.setItem('11am', 'LEavfework')
-testEl = localStorage.getItem('hour-9')
-console.log(testEl)
+// var testEls = document.getElementById('#input-9')
+// document.getElementById('#input-9').innerHTML = localStorage.input
+// console.log(testEls)
+// console.log(localStorage.input)
+// localStorage.setItem('10am', 'Go to Work')
+// localStorage.setItem('hour-9', 'textarea')
+// localStorage.setItem('11am', 'LEavfework')
+// testEl = localStorage.getItem('hour-9')
+// console.log(testEl)
 
 // function addTextEntry(key, text, isNewEntry) {
 
@@ -81,8 +83,144 @@ console.log(testEl)
 //     addTextEntry(key, text, isNewEntry)
 
 // localStorage.input = "";
-document.getElementById("input").innerText = localStorage.input;
-console.log(localStorage.input)
-document.getElementById("button-9").addEventListener("click", function(){
-    localStorage.setItem("hour-9", localStorage.input);
-});
+// document.getElementById("input-9").innerHTML = localStorage.input;
+// console.log(localStorage.input)
+// document.getElementById("button-9").addEventListener("click", function(){
+//     document.getElementById("input-9").textContent = localStorage.input;
+//     localStorage.setItem("hour-9", localStorage.input);
+// });
+
+var input_textarea = document.querySelector('.description-9');
+var output_div = document.querySelector('#input-9');
+var save_button = document.querySelector('.btn');
+
+save_button.addEventListener('click', updateOutput);
+
+output_div.textContent = localStorage.getItem('hour-9');
+input_textarea.value = localStorage.getItem('hour-9');
+
+function updateOutput() {
+	localStorage.setItem('hour-9', input_textarea.value);
+	
+	output_div.textContent = input_textarea.value;
+}
+
+var input_textarea2 = document.querySelector('.description-10');
+var output_div2 = document.querySelector('#input-10');
+var save_button2 = document.querySelector('.btn');
+
+save_button2.addEventListener('click', updateOutput);
+
+output_div2.textContent = localStorage.getItem('hour-10');
+input_textarea2.value = localStorage.getItem('hour-10');
+
+function updateOutput() {
+	localStorage.setItem('hour-10', input_textarea2.value);
+	
+	output_div2.textContent = input_textarea2.value;
+}
+
+var input_textarea3 = document.querySelector('.description-11');
+var output_div3 = document.querySelector('#input-11');
+var save_button3 = document.querySelector('.btn');
+
+save_button3.addEventListener('click', updateOutput);
+
+output_div3.textContent = localStorage.getItem('hour-11');
+input_textarea3.value = localStorage.getItem('hour-11');
+
+// function updateOutput() {
+// 	localStorage.setItem('hour-11', input_textarea3.value);
+	
+// 	output_div3.textContent = input_textarea3.value;
+// }
+
+// var input_textarea4 = document.querySelector('.description-12');
+// var output_div4 = document.querySelector('#input-12');
+// var save_button4 = document.querySelector('.btn');
+
+// save_button4.addEventListener('click', updateOutput);
+
+// output_div4.textContent = localStorage.getItem('hour-12');
+// input_textarea4.value = localStorage.getItem('hour-12');
+
+// function updateOutput() {
+// 	localStorage.setItem('hour-12', input_textarea4.value);
+	
+// 	output_div4.textContent = input_textarea4.value;
+// }
+
+// var input_textarea5 = document.querySelector('.description-13');
+// var output_div5 = document.querySelector('#input-13');
+// var save_button5 = document.querySelector('.btn');
+
+// save_button5.addEventListener('click', updateOutput);
+
+// output_div5.textContent = localStorage.getItem('hour-13');
+// input_textarea5.value = localStorage.getItem('hour-13');
+
+// function updateOutput() {
+// 	localStorage.setItem('hour-13', input_textarea5.value);
+	
+// 	output_div5.textContent = input_textarea5.value;
+// }
+
+// var input_textarea6 = document.querySelector('.description-14');
+// var output_div6 = document.querySelector('#input-14');
+// var save_button6 = document.querySelector('.btn');
+
+// save_button6.addEventListener('click', updateOutput);
+
+// output_div6.textContent = localStorage.getItem('hour-14');
+// input_textarea6.value = localStorage.getItem('hour-14');
+
+// function updateOutput() {
+// 	localStorage.setItem('hour-14', input_textarea6.value);
+	
+// 	output_div6.textContent = input_textarea6.value;
+// }
+
+// var input_textarea7 = document.querySelector('.description-15');
+// var output_div7 = document.querySelector('#input-15');
+// var save_button7 = document.querySelector('.btn');
+
+// save_button7.addEventListener('click', updateOutput);
+
+// output_div7.textContent = localStorage.getItem('hour-15');
+// input_textarea7.value = localStorage.getItem('hour-15');
+
+// function updateOutput() {
+// 	localStorage.setItem('hour-15', input_textarea7.value);
+	
+// 	output_div7.textContent = input_textarea7.value;
+// }
+
+// var input_textarea8 = document.querySelector('.description-16');
+// var output_div8 = document.querySelector('#input-16');
+// var save_button8 = document.querySelector('.btn');
+
+// save_button8.addEventListener('click', updateOutput);
+
+// output_div8.textContent = localStorage.getItem('hour-16');
+// input_textarea8.value = localStorage.getItem('hour-16');
+
+// function updateOutput() {
+// 	localStorage.setItem('hour-16', input_textarea8.value);
+	
+// 	output_div8.textContent = input_textarea8.value;
+// }
+
+// var input_textarea9 = document.querySelector('.description-17');
+// var output_div9 = document.querySelector('#input-17');
+// var save_button9 = document.querySelector('.btn');
+
+// save_button9.addEventListener('click', updateOutput);
+
+// output_div9.textContent = localStorage.getItem('hour-17');
+// input_textarea9.value = localStorage.getItem('hour-17');
+
+// function updateOutput() {
+// 	localStorage.setItem('hour-17', input_textarea9.value);
+	
+// 	output_div9.textContent = input_textarea9.value;
+// }
