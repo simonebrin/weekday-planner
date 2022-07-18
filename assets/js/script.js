871d047cbc135dd53d195020f819bc4b9a6a3f59
Simone Brin <simonebrin@gmail.com>
2022-07-18 12:43:50 -0400

    added last comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,15 @@
+//Show current date
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-var myTimeBlock = document.getElementsByClassName('time-block')
-// var myTimeBlock = document.getElementsById()
-// var myTimeBlock = document.getElementsById(parseInt(myTimeBlock[i].id.substring))
-// var myTimeBlock = document.getElementsByClassName('hour')
-// var myTimeBlock = document.getElementsByClassName('datetime')
-// var myTimeBlock = document.querySelector(".hour")
-// var myTimeBlock = document.querySelector(".hour")
-// var timeBackground = document.querySelector('time-block')
 var currentDay = moment().format("MMM Do YY")
 var currentHour = moment().hour()
-// var currentHour = moment().format("HH mm")
+
+// Determine time and add/remove css class accordinly
+var myTimeBlock = document.getElementsByClassName('time-block')
 console.log(currentHour)
 console.log(currentDay)
 function checkTime() {
     for (let i = 0; i < myTimeBlock.length; i++) {
-        //currentHour is going to equal 11
         console.log(myTimeBlock[i].textContent, currentHour)
         console.log(parseInt(myTimeBlock[i].id.substring(5)))
         if ((parseInt(myTimeBlock[i].id.substring(5))) < currentHour) {
@@ -29,13 +23,11 @@ function checkTime() {
             myTimeBlock[i].classList.remove('present')
             myTimeBlock[i].classList.remove('past')
         }
-       
-
     }
 }
 checkTime();
 
-
+// Save button functionality and local storage save
 $('.saveBtn').on('click', function() {
     var value = $(this).siblings('.description').val()
     var time = $(this).siblings('.description').attr('id')
